08331b2d2c1780af7abf56c74cc2731970343798
Sharian Johnson <shariant98@gmail.com>
2025-03-01 18:55:49 -0500

    completed lab

--- app/config.py
+++ app/config.py
@@ -6,4 +6,8 @@ load_dotenv()
 class Config(object):
     """Base Config Object"""
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    SECRET_KEY = 'Som3$ec5etK*y'
+    MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USERNAME = '072e08940df9d4'
+    MAIL_PASSWORD = '0556fcec35c376'
